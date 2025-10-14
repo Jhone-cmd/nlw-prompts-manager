55dc9a67b270d5be04c0110ae3df15d064f01e69
Jhone-cmd <jhone.1941@gmail.com>
2025-10-14 11:23:36 -0300

    add functionality to open and close the menu (sidebar)

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const elements = {
   promptContent: document.getElementById("prompt-content"),
   titleWrapper: document.getElementById("title-wrapper"),
   contentWrapper: document.getElementById("content-wrapper"),
+  btnOpen: document.getElementById("btn-open"),
+  btnCollapse: document.getElementById("btn-collapse"),
+  sidebar: document.querySelector(".sidebar"),
 }
 
 // Atualiza o estado do wrapper conforme o conteúdo do elemento
@@ -30,11 +33,30 @@ function attachAllEditableHandlers() {
   })
 }
 
+// Funções para abrir e fechar a sidebar
+function openSidebar() {
+  elements.sidebar.style.display = "flex"
+  elements.btnOpen.style.display = "none"
+}
+
+function closeSidebar() {
+  elements.sidebar.style.display = "none"
+  elements.btnOpen.style.display = "block"
+}
+
 
 // Inicialização
 function init() {
   attachAllEditableHandlers()
   updateAllEditableStates()
+  
+ // Estado inicial: sidebar aberta, botão de abrir oculto
+  elements.sidebar.style.display = ""
+  elements.btnOpen.style.display = "none"
+
+  // Eventos para abrir/fechar sidebar
+  elements.btnOpen.addEventListener("click", openSidebar)
+  elements.btnCollapse.addEventListener("click", closeSidebar)
 }
 
 // Executa a inicialização ao carregar o script
